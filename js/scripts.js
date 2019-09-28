73e0134bf7be8e9b91d2c94db12549154387b373
mervinkogei <vincent.kogei@yahoo.com>
2019-09-28 11:41:15 +0300

    add the jquery to validate the form and display message on submit

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,12 @@ $(document).ready(function() {
     $("#design3").toggle();
       });
 });
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    var name=$("input#name").val();
+  alert("Hello "+ name + " Thank you for contacting us");
+
+  event.preventDefault();
+  });
+ });
