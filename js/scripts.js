f784eee2fc0dd499a35187c646bd5c744a88f434
mervinkogei <vincent.kogei@yahoo.com>
2019-09-29 21:56:10 +0300

    add the toggle effect and hide on What we do div class

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,48 @@
 $(document).ready(function() {
   $(".img1").click(function() {
     $("#design1").toggle();
-      });
+    $(".img1").toggle();
+  });
 });
+$(document).ready(function() {
+  $(".design").click(function() {
+    $(".img1").show();
+    $("#design1").hide();
+  });
+});
+
 $(document).ready(function() {
   $(".img2").click(function() {
     $("#design2").toggle();
-      });
+    $(".img2").toggle();
+  });
 });
+$(document).ready(function() {
+  $(".development").click(function() {
+    $(".img2").show();
+    $("#design2").hide();
+  });
+});
+
 $(document).ready(function() {
   $(".img3").click(function() {
     $("#design3").toggle();
-      });
+    $(".img3").toggle();
+  });
 });
 
-$(document).ready(function(){
-  $("form").submit(function(event){
-    var name=$("input#name").val();
-  alert("Hello "+ name + " Thank you for contacting us");
-
-  event.preventDefault();
+$(document).ready(function() {
+  $(".product").click(function() {
+    $(".img3").show();
+    $("#design3").hide();
   });
- });
+});
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var name = $("input#name").val();
+    alert("Hello " + name + " Thank you for contacting us");
+
+    event.preventDefault();
+  });
+});
